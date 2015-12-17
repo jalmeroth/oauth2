e1af08cca41840f290723463ab3c04f90fae65a9
Jan Almeroth <jan@MBPr-ACBC3285A151.local>
2015-12-17 20:10:01 +0100

    Introduced API-Key param

--- helpers.py
+++ helpers.py
@@ -5,10 +5,14 @@ logger = logging.getLogger(__name__)
 from request import WebRequest
 
 
-def shorten(longUrl):
+def shorten(longUrl, api_key = None):
     """Find documentation here: https://developers.google.com/url-shortener/v1/getting_started#shorten"""
     
     uri = 'https://www.googleapis.com/urlshortener/v1/url'
+    
+    params = {}
+    if api_key:
+        params['key'] = api_key
     
     headers = {
         'Content-Type': 'application/json'
@@ -19,7 +23,7 @@ def shorten(longUrl):
     }
     
     r = WebRequest()
-    result = r.post(uri, headers=headers, data=json.dumps(payload))
+    result = r.post(uri, headers=headers, data=json.dumps(payload), params=params)
     data = r.json()
     
     return data.get('id')
